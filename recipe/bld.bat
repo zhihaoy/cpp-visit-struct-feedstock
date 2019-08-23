fe29cb70744f6ca5b3d584862470a7fa01a4f6cd
Zhihao Yuan <zy@simplerose.com>
2019-08-23 12:09:57 -0500

    feat: add visit-struct recipe

---
+++ recipe/bld.bat
@@ -0,0 +1,8 @@
+cmake -G Ninja ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  %SRC_DIR%
+if errorlevel 1 exit 1
+
+ninja install
+if errorlevel 1 exit 1
---
+++ recipe/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+_cmake_config=(
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+    -DCMAKE_INSTALL_LIBDIR=lib
+    -DCMAKE_SKIP_INSTALL_RPATH=ON
+    -DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}"
+    -DCMAKE_BUILD_TYPE=Release
+)
+
+cmake -G Ninja "${_cmake_config[@]}" "${SRC_DIR}"
+
+ninja install
